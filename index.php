eaaf88d3edc3ceb2ea694588d64615ee397eea43
salmaforex <gundambison@gmail.com>
2016-02-03 13:44:09 +0700

    production

--- index.php
+++ index.php
@@ -54,9 +54,7 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT',"production");
-//	isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
-//	define('_DEV_',1); //hapus jika bukan Development
+	define('ENVIRONMENT',"production"); 
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
