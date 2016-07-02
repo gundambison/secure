cf3f420bfee9b27ecf9c4168dd1e16d3db765918
salmaforex <gundambison@gmail.com>
2016-07-02 14:02:21 +0700

    fix alamat dan menu

--- application/views/depan/inc/menu_admin_view.php
+++ application/views/depan/inc/menu_admin_view.php
@@ -15,7 +15,7 @@
 	<a class="menuitem submenuheader" href="#" ><span class="glyphicon glyphicon-user"></span> ADMIN TOOLS</a>
 	<div class="submenu">
 		<ul>
-			<li><a href="<?=base_url('member/listApi');?>"> <img class="ui--icon" src="<?=base_url();?>media/img/open-account.png" alt="" data-at2x="size:14px" data-retina-auto="1" style="margin-right: 5px;"> API</a></li>
+			<li><a href="<?=base_url('member/listApi/api');?>"> <img class="ui--icon" src="<?=base_url();?>media/img/open-account.png" alt="" data-at2x="size:14px" data-retina-auto="1" style="margin-right: 5px;"> API</a></li>
             <li><a href="<?=!isset($detail['id'])?base_url("member/logout"):base_url("member/listApi/deposit");?>"> <img class="ui--icon" src="<?=base_url();?>media/img/open-account.png" alt="" data-at2x="size:14px" data-retina-auto="1" style="margin-right: 5px;"><?php 
 					echo isset($detail['id'])?'DAFTAR DEPOSIT':'unknown';?>
 		
@@ -38,6 +38,6 @@
 			</a></li>
 		</ul>
 	</div>
-	<a class="menuitem" href="<?=base_url('member/logout');?>" ><span class="glyphicon glyphicon-log-out"></span> Logout</a>
+
 	
 </div>
