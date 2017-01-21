1ef22f2b4e1b2a4cd42d39e3562c98cbc06e51df
salmaforex <gundambison@gmail.com>
2017-01-21 12:19:37 +0700

    erase config

--- application/config/forexConfig.php
+++ application/config/forexConfig.php
@@ -20,38 +20,12 @@ if(defined('LOCAL')){
 	$config['forexKey']='unknown99';
 }
 else{ 
-	$config['urlForex']=array( 
-		'default'=>			'https://www.natureforex.com/rest-api/salma' ,
-		'register'=>		'https://www.natureforex.com/rest-api/salma/register',
-		'update'=>			'https://www.natureforex.com/rest-api/salma/update-account' ,
-		'updateBalance'=>	'https://www.natureforex.com/rest-api/salma/update-balance'
-	);
-	
-	$config['api_url']='http://secure.salmaforex.com/forex/api';
-	$config['forexKey']='SalmaFX123!@#';
-	
+/* null */
 }
 
 //if dev
 if(defined('_DEV_')){
-	$config['api_url']='http://dev.salmaforex.com/forex/api';
-	$config['urlForex']['updateBalance']=
-	  'http://dev.salmaforex.com/forex/fake/updateBalance';
-}else{} 
-
-$config['urlForex']['local']='http://advance.forex/index.php/forex/data';
-
-$config['forexBank']=array(
-	array('name'=>'BCA', 'number'=>'281.222.6.160','person'=>'PT. Salma Widyatama Mandiri'),
-	array('name'=>'BRI', 'number'=>'10700.1000.1953.04','person'=>'PT. Salma Widyatama Mandiri'),
-	array('name'=>'MANDIRI', 'number'=>'13000.2323.1999','person'=>'PT. Salma Widyatama Mandiri'),
-	array('name'=>'BNI', 'number'=>'30.1212.3020','person'=>'PT. Salma Widyatama Mandiri'),
-
-);
 /*
-BCA : 2812226160 a.n PT. Salma Widyatama Mandiri 
-BRI : 10700.1000.1953.04 a.n Yadi Supriyadi
-MANDIRI : 1300023231999 a.n PT. Salma Widyatama Mandiri
-BNI : 3012123020 a.n PT. Salma Widyatama Mandiri
+null
 */
-$config['sendpulse_pubkey']='';
+}else{} 
