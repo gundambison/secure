542154c31bc7b652c02fd28360ea84b931385707
Gunawan Wibisono <gundambison@gmail.com>
2017-04-10 16:28:42 +0700

    backup.. new table

--- application/views/api/backup_data.php
+++ application/views/api/backup_data.php
@@ -9,6 +9,19 @@ $aSql[]="create table IF NOT EXISTS {$tableTarget} like `mujur_api`;";
 $aSql[]="insert into  {$tableTarget} select z1.* from {$tableTarget} z2 right join mujur_api z1 on z1.id=z2.id where z2.id is null ;";
 $aSql[]="delete from `mujur_api` where created < '{$late2days}';";
 $aSql[]="optimize table `mujur_api`";
+
+$sql="select count(*) from {$tableTarget}";
+$data=dbFetchOne($sql);
+if($data['c'] > 100000){
+	$date=date("Ymd");
+	$aSql[]="alter table `{$tableTarget}` RENAME TO `{$tableTarget}_{$date}`";
+	$aSql[]="create table `{$tableTarget}` like mujur_api";
+}
+/*
+ALTER TABLE z_api RENAME TO z_api_170410;
+create table z_api like mujur_api;
+*/
+
 echo implode("\n",$aSql);
 foreach($aSql as $sql){
 	dbQuery($sql,true);
